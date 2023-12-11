93a1599e64c677791050c85448411dba0a5eefdf
Luan van Pletsen <luanvanpletsen@gmail.com>
2023-12-11 20:34:32 +0000

    Implement traditional self-attn

--- toy_flash_attn/_modidx.py
+++ toy_flash_attn/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://Luan-vP.github.io',
                 'git_url': 'https://github.com/Luan-vP/toy_flash_attn',
                 'lib_path': 'toy_flash_attn'},
-  'syms': {'toy_flash_attn.core': {'toy_flash_attn.core.foo': ('core.html#foo', 'toy_flash_attn/core.py')}}}
+  'syms': { 'toy_flash_attn.core': {'toy_flash_attn.core.foo': ('core.html#foo', 'toy_flash_attn/core.py')},
+            'toy_flash_attn.transformer': {'toy_flash_attn.transformer.foo': ('transformer.html#foo', 'toy_flash_attn/transformer.py')}}}
